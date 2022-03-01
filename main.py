d0f9d4bb51a75b54de0e887e294d311ae08d6158
Oscar Capraro <oscarc@tinlion.com>
2022-03-01 13:47:29 -0800

    feat: slash commands

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-import discord
 import os
-from discord.ext import commands
 from dotenv import load_dotenv
+import sqlite3
+import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 
-load_dotenv()  # take environment variables from .env.
+# Take environment variables from .env
+load_dotenv()  
+
+
+
 
 # Vars
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix='rose!', intents=intents, help_command = None)
+client = commands.Bot(command_prefix='rose!', intents=intents)
+slash = SlashCommand(client, sync_commands=True)
 
 # On Start
 @client.event
@@ -15,4 +23,22 @@ async def on_ready():
   print('We have logged in as {0.user}'.format(client))
   await client.change_presence(activity = discord.Game('rose!help'))
 
-client.run(os.environ.get('BOT'))
+# Slash Commands
+@slash.slash(
+  name="getuser",
+  description="Get user ID",
+  guild_ids=[685164560948527164],
+  options=[
+    create_option(
+      name="user",
+      description="Select a user",
+      required=True,
+      option_type=6
+    )
+  ]
+)
+async def _getuser(ctx:SlashContext, user:str):
+  await ctx.send(user.id)
+
+# Run
+client.run(os.environ.get('BOT_TOKEN'))
