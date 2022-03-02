5c3871c4ab68eb109cd0c1e44d896e56bce49d3e
Oscar Capraro <oscarc@tinlion.com>
2022-03-02 10:03:55 -0800

    feat: create .env file if not present

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from discord.ext import commands, tasks
 from discord_slash import SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_choice, create_option
 
+# Creates .env file if not present
+if not os.path.exists(".env"):
+  open(".env", 'w+')
+
 # Take environment variables from .env
 load_dotenv()  
 
